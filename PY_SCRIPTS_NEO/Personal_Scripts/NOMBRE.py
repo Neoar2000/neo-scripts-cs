cd62b41bbcaa0e7ad0792bac2cceddda036d61e7
Neoar2000 <josephneoar@gmail.com>
2023-04-08 18:34:14 -0400

    change

--- PY_SCRIPTS_NEO/Personal_Scripts/NOMBRE.py
+++
@@ -1,6 +0,0 @@
-nombre = input("Introduzca su nombre: ")
-while nombre != nombre.isalpha:
-    if not nombre.isalpha():
-        print("Valor errado. Intente nuevamente")
-    else:
-        print("Hola " + nombre + "!")
