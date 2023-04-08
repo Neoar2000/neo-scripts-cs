c33a4b8ae791d6b085264698359960870850e284
Neo Aliaga <josephneoar@gmail.com>
2023-04-08 13:22:54 -0400

    change

--- PY_SCRIPTS_NEO/CONTRASEÑA PYTHON NEO.py
+++
@@ -1,9 +0,0 @@
-import time
-pwd = str(input("Ingrese su contraseña: "))
-
-while pwd != "hola":
-    time.sleep(1)
-    print("Contraseña incorrecta. Intente de nuevo.")
-    pwd = str(input("Ingrese su contraseña: "))
-
-print("Bienvenido de nuevo, Neo!")
