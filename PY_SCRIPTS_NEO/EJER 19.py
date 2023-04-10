17ab262f04a58b3f772e77dcc061e52a7ee5ab9d
Neo <josephneoar@gmail.com>
2023-04-10 16:39:31 -0400

    final

---
+++ PY_SCRIPTS_NEO/EJER 19.py
@@ -0,0 +1,15 @@
+com = 0
+sum = 0
+x = 2
+
+while com <= 100:
+    primo = True
+    for i in range(2, x // 2):
+        if x % i == 0:
+            primo = False
+            break
+    if primo == False:
+        com += 1
+    sum += x**2
+    x = x + 1
+    print(sum)
