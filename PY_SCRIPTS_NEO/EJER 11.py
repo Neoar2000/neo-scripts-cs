b93fe75efeead18e4bdefcb2b14b2257abcb2060
Neo <josephneoar@gmail.com>
2023-04-10 16:16:46 -0400

    new file

---
+++ PY_SCRIPTS_NEO/EJER 11.py
@@ -0,0 +1,8 @@
+f1 = int(input("Introduce el primer factor: "))
+f2 = int(input("Introduce el segundo factor: "))
+sum = 0
+
+for i in range(f2):
+    sum = sum + f1
+
+print("El resultado de la multiplicación es:", sum)
