66738cdda0ec58fbb68411368cf0247a5dc53f05
Neo Aliaga <josephneoar@gmail.com>
2023-04-14 15:40:51 -0400

    change

--- PY_SCRIPTS_NEO/CLASE 14-04-2023.py
+++
@@ -1,4 +0,0 @@
-a = ['A', 1, True, (4, 5), 0]
-print(type(a))
-t = tuple(a)
-print(type(t))
---
+++ PY_SCRIPTS_NEO/TUPLAS CLASE.py
@@ -0,0 +1,8 @@
+frutas = "manzana", "banana", "pera", "sandia", "melon", "palta"
+print(type(frutas))
+print(frutas)
+
+rojo,amarillo,*verde = frutas
+print(rojo)
+print(amarillo)
+print(verde)
