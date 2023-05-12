2703d3d1f79f2df07ecd8cf1161b1125574927f3
Neo Aliaga <josephneoar@gmail.com>
2023-05-12 15:37:20 -0400

    new files and modifications

---
+++ PY_SCRIPTS_NEO/RESOLUCION EXAMEN PT2.py
@@ -0,0 +1,8 @@
+a = 0
+b = 0
+c = 0
+
+while not (a + 7 == b or b + 7 == c  and c % 7 == 0):
+    a = int(input("Ingrese tres multiplos de 7 consecutivos ascendentes: "))
+    b = int(input("Ingrese tres multiplos de 7 consecutivos ascendentes: "))
+    c = int(input("Ingrese tres multiplos de 7 consecutivos ascendentes: "))
