7e465810bf0e990c9b37dde5101078bfd255516e
Neo Aliaga <josephneoar@gmail.com>
2023-06-28 14:14:19 -0400

    New files added

---
+++ C_C++_SCRIPTS_NEO/PERSONAL_SCRIPTS/TEST_VARIABLES.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char nombre[] = "Neo";
+    int edad = 23;
+    char categoria = 'A';
+    double nota = 82.80;
+
+    printf("Hola! Mi nombre es %s.\n", nombre);
+    printf("Mi edad es de %d anios.\n", edad);
+    printf("Soy de categoria %c.\n", categoria);
+    printf("Mi nota es de %f.\n", nota);
+
+    return 0;
+}
