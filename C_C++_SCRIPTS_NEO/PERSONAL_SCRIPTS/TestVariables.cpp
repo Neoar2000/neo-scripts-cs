a9425416e0af7096e2f1c75cf9c47b5e80b87c95
Neo Aliaga <josephneoar@gmail.com>
2023-07-31 10:40:10 -0400

    New files added

---
+++ C_C++_SCRIPTS_NEO/PERSONAL_SCRIPTS/TestVariables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nombre = "Neo";
+    int edad = 50;
+    cout << "Hola, " << nombre << " Tienes " << edad << " anios de edad." << endl;
+    return 0;
+}
