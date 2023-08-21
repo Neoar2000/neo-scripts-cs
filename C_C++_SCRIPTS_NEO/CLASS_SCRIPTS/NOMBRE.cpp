4855829d5f8524a170bf685fc452276ec3660e07
Neo Aliaga <josephneoar@gmail.com>
2023-08-21 18:43:54 -0400

    New files added, Java folder created

---
+++ C_C++_SCRIPTS_NEO/CLASS_SCRIPTS/NOMBRE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    char nombreCompleto[10];
+    cout << "Ingrese su nombre completo: ";
+    cin >> nombreCompleto;
+    cout << "Su nombre completo es: " << nombreCompleto << endl;
+    return 0;
+}
